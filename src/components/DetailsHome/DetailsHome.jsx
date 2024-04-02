10cf3f5cdbf2566f6675a4c221a00f9185ccfe58
“horia-abdaljabbar” <horiahaji.77sr@gmail.com>
2024-04-02 11:35:39 +0300

    update home page

--- src/components/DetailsHome/DetailsHome.jsx
+++ src/components/DetailsHome/DetailsHome.jsx
@@ -16,7 +16,7 @@ function DetailsHome() {
       <FontAwesomeIcon icon={faHandshake} style={{background: "#cb9696",}}/>
               <div className="card-body">
           <h5 className="card-title">Amazing Deals</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">We provide you with amazing products at best prices and many payment ways.</p>
           {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
@@ -25,7 +25,7 @@ function DetailsHome() {
       <div className="card" style={{background: "#dcebdd",}}>
       <FontAwesomeIcon icon={ faThumbsUp} style={{background: "#8bbd8e",}} />        <div className="card-body">
           <h5 className="card-title">Quality Products</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">We offer a wide range of high-quality and great products</p>
           {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
@@ -34,7 +34,7 @@ function DetailsHome() {
       <div className="card" style={{background: "#f5efd8",}}>
       <FontAwesomeIcon icon={ faPeopleGroup} style={{background: "#d1b54a",}} />        <div className="card-body">
           <h5 className="card-title">For All</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">shopping with us with your family ,we offer products for all ages.</p>
           {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
@@ -43,7 +43,7 @@ function DetailsHome() {
       <div className="card" style={{background: "#f4e6d8",}}>
       <FontAwesomeIcon icon={faHeadphones} style={{background: "#d29a61",}} />        <div className="card-body">
           <h5 className="card-title">Best Support</h5>
-          <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card's content.</p>
+          <p className="card-text">we provide services 24/7.contact with us at any time</p>
           {/* <a href="#" className="btn btn-primary">Go somewhere</a> */}
         </div>
       </div>
